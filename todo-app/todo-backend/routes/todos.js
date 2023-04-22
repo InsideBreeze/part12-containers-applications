39ef3a5f14f9506367dc4d1e1c3236528d106259
zephyr <linjie.zhang@outlook.com>
2023-04-22 16:31:54 +0800

    12.5-12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,18 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = req.todo;
+  res.json(todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const todo = req.todo
+  const newTodo = await Todo.findByIdAndUpdate(todo.id, {
+    text: req.body.text,
+    done: req.body.done
+  }, { new: true })
+  return res.json(newTodo)
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
